28affc224d9a473b3187028c88c7ede7814e4440
Will Robinson <wgr@slurm.dcs.aber.ac.uk>
2023-04-18 04:53:00 +0100

    extended configs for new model setups

---
+++ classification/configs/quadtree/t2.py
@@ -0,0 +1,6 @@
+cfg = dict(
+    model='quadtree_t2',
+    drop_path=0.1,
+    clip_grad=None,
+    output_dir='checkpoints/quadtree_t2'
+)
---
+++ classification/configs/quadtree/t4.py
@@ -0,0 +1,6 @@
+cfg = dict(
+    model='quadtree_t4',
+    drop_path=0.1,
+    clip_grad=None,
+    output_dir='checkpoints/quadtree_t4'
+)
